dd51d2cc656d0e6c33447c24998d1549d81b4de3
chr7stos <sikkis@pm.me>
2020-06-13 19:11:41 +0200

    Added month selection

--- streamlit_cy_water_inflow.py
+++ streamlit_cy_water_inflow.py
@@ -5,21 +5,19 @@ from vega_datasets import data
 
 st.title("Monthly water inflows since 1988 in Cyprus")
 st.markdown("""
-This is a dashboard with data **from data.gov.cy** and you can get them [here](https://www.data.gov.cy/dataset/μηνιαία-εισροή-νερού-στους-ταμιευτήρες-νερού-φράγματα)
+This is a dashboard with data data.gov.cy **data.gov.cy** and you can get them [here](https://www.data.gov.cy/dataset/μηνιαία-εισροή-νερού-στους-ταμιευτήρες-νερού-φράγματα).   
+The goal is to have easy simple visualisations for more of the open data in Cyprus.
 """)
 
 df = st.cache(pd.read_csv)("data/monthly_water_inflows_from_1988.csv")
 
-months = st.sidebar.multiselect('Show availavle months', df.columns[1:-1])
+month = st.selectbox('Show available months', df.columns[1:], 0)
 
-#st.markdown("This is cool!")
-#st.line_chart(df["TOTAL"].dropna())
-
-#st.altair_chart(df[" OCTOBER"].dropna())
+st.markdown("Flow of water in Cyprus dams")
 
 chart = alt.Chart(df).mark_line().encode(
     x='YEAR',
-    y=' OCTOBER',
+    y=month
 ).interactive()
 
 st.altair_chart(chart)
